d901721614609ae4d0d345bc68972529bbe5f831
KRAZYFLASH <muhammadfakhri.hsb@gmail.com>
2022-10-03 22:02:28 +0700

    wkwk

--- cafeloka/app/Views/welcome_message.php
+++ cafeloka/app/Views/welcome_message.php
@@ -1,1 +1,5 @@
 <h1>Hallo Joy</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate voluptatibus at cumque tempora ipsum!</p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum eveniet est placeat dignissimos non consequatur accusantium eligendi dicta doloribus.</p>
