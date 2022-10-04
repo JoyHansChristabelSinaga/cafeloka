65bc88dce48bfb49c0aec9c18936df070fbf9f1a
JoyHansChristabelSinaga <83484843+JoyHansChristabelSinaga@users.noreply.github.com>
2022-10-04 15:55:22 +0700

    navigation bar

--- cafeloka/app/Views/vw_register.php
+++ cafeloka/app/Views/vw_register.php
@@ -11,6 +11,25 @@
     <title>Hello, world!</title>
   </head>
   <body class="d-flex flex-column h-100">
+  <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/home">Cafeloka</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" aria-current="page" href="/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/register">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
 <!-- Begin page content -->
 <main class="flex-shrink-0">
